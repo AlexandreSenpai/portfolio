6158ac7b0b146d45aa2830a7933ac7e1bbc049c4
Alexandre Ramos <alexandre_brito_de_souza_ramos_hvar@whirlpool.com>
2021-10-24 05:59:36 -0300

    feat: added environment variables to point to backend host

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL: process.env.BACKEND_HOST || process.env.NEXT_PUBLIC_BACKEND_HOST,
 });
